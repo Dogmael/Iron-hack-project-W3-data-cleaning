843b25ce21a61e4ad53d3abefa7b6a1962142c25
Dogmaël DARTIGUE-PEYROU <dogmaeldogmael@gmail.com>
2023-07-24 12:14:21 +0200

    After presentation

--- data_manipulations.sql
+++ data_manipulations.sql
@@ -1,5 +1,4 @@
 USE datacleaning;
-
 -- Create a table with cyberbullier acount informations 
 CREATE TABLE cyberbullies_acount_profil AS
 SELECT SenderId,
@@ -11,7 +10,6 @@ SELECT SenderId,
     SenderLocation
 FROM main
 WHERE IsCyberbullying = TRUE;
-
 -- Create a table with regulare user acount informations 
 CREATE TABLE no_cyberbullies_acount_profil AS
 SELECT SenderId,
@@ -23,21 +21,19 @@ SELECT SenderId,
     SenderLocation
 FROM main
 WHERE IsCyberbullying = FALSE;
-
 -- Calculate the average message length for cyberbullying and non-cyberbullying incidents:
 SELECT IsCyberbullying,
-    AVG(LENGTH(Message)) AS AvgMessageLength
+    AVG(LENGTH(Text)) AS AvgMessageLength
 FROM main
 GROUP BY IsCyberbullying;
-
 -- Identify the most common words in cyberbullying messages:
 SELECT SUBSTRING_INDEX(
-        SUBSTRING_INDEX(Message, ' ', n.digit + 1),
+        SUBSTRING_INDEX(Text, ' ', n.digit + 1),
         ' ',
         -1
     ) AS word,
     COUNT(*) AS frequency
-FROM cyberbullying_data
+FROM main
     JOIN (
         SELECT 0 AS digit
         UNION ALL
@@ -46,22 +42,23 @@ FROM cyberbullying_data
         SELECT 2
         UNION ALL
         SELECT 3
-    ) n ON LENGTH(Message) - LENGTH(REPLACE(Message, ' ', '')) >= n.digit
+    ) n ON LENGTH(Text) - LENGTH(REPLACE(Text, ' ', '')) >= n.digit
 WHERE IsCyberbullying = 1
 GROUP BY word
 ORDER BY frequency DESC
 LIMIT 10;
-
 -- You can adjust the limit to get more or fewer common words
-SenderFollowings, SenderFollowers, SenderStatues, SenderLocation
+SELECT SenderFollowings,
+    SenderFollowers,
+    SenderStatues,
+    SenderLocation
 FROM main
 WHERE IsCyberbullying = TRUE;
-
 -- Find the top cyberbullying users (those with the highest number of identified cyberbullying incidents)
-SELECT User,
+SELECT Id,
     COUNT(*) AS NumOfCyberbullyingIncidents
-FROM cyberbullying_data
+FROM main
 WHERE IsCyberbullying = 1
-GROUP BY User
+GROUP BY Id
 ORDER BY NumOfCyberbullyingIncidents DESC
 LIMIT 10; --  adjusting the limit to get more or fewer top users
